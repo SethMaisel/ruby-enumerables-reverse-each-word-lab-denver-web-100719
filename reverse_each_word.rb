1849f9f0b41d36c34788c1df1f0f262d84c340c4
Seth Maisel <seth.maisel@gmail.com>
2019-10-02 18:19:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (string_array)
   array_to_reverse = string_array.split (" ")
   reversed_string_array = array_to_reverse.each do {|x| x.reverse!}
-  return reversed_string
+  return reversed_string_array
     
   end
